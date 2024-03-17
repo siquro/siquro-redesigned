d3ce458255544d68fb0ab621181e5b6a88b86b6b
IlonaKuzmina-GWM <ilona@greenwiremedia.com>
2024-03-17 13:01:35 +0200

    .

--- src/app/api/email/route.tsx
+++ src/app/api/email/route.tsx
@@ -2,7 +2,8 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 import WelcomeEmail from '../../../emails/welcome';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_WL4rMVDM_BYqcCb7vVLF6GDvponEh2GQf');
 
 export async function POST(request: Request) {
     const { name, message, email } = await request.json();
