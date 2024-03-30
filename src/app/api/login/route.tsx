d4cdbb1ce605f67f63375c71a8377710f050438f
IlonaKuzmina-GWM <ilona@greenwiremedia.com>
2024-03-30 14:53:08 +0200

    TEST 6

--- src/app/api/login/route.tsx
+++ src/app/api/login/route.tsx
@@ -75,15 +75,15 @@ export async function POST(request: NextRequest, response: NextResponse) {
 
   const data = await res.json();
 
-  const cookie = serialize('token', data.token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
-    path: '/',
-    maxAge: 60 * 60 * 24 * 7, // 7 days
-  });
+  // const cookie = serialize('token', data.token, {
+  //   httpOnly: true,
+  //   secure: process.env.NODE_ENV === 'production',
+  //   sameSite: 'lax',
+  //   path: '/',
+  //   maxAge: 60 * 60 * 24 * 7, // 7 days
+  // });
 
-  response.headers.set('Set-Cookie', cookie);
+  // response.headers.set('Set-Cookie', cookie);
 
 
   return NextResponse.json({ success: true, redirectUrl: 'https://pay.siquro.com/auth/login' });
